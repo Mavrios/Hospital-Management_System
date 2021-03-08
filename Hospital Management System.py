d61077b53f86c0db2894e0d7bbe399869de33b41
Ahmed Kishk <ahmedadelkishk@gmail.com>
2021-03-08 20:40:35 +0200

    Add More Feature to Add Patient

--- Hospital Management System.py
+++ Hospital Management System.py
@@ -1,12 +1,32 @@
 import tkinter
+from tkinter import font
+from tkinter.constants import CENTER, E, N, NE, W
 from PIL import ImageTk, Image
+import json
+import ast
 SecurityData = list()
+Departments  = list()
+DepartmentsStrip = list()
+DoctorName = list()
+DepartmentsFile = open("Departments.cfg" , "r")
+Departments = DepartmentsFile.readlines()
+for i in Departments:
+    DepartmentsStrip.append(i.rstrip())
 
+print(DepartmentsStrip)
 AdminFile = open("Admin" , "r")
 SecurityData = AdminFile.readlines()
 print(SecurityData)
 AdminFile.close()
 Counter = 2
+
+DatabaseFile = open("Database" , "r")
+Context = DatabaseFile.read()
+Database = ast.literal_eval(Context)
+print(Database)
+for i in Database['Anesthetics']['Doctor']:
+    DoctorName.append("Dr. " + i["Name"])
+
 
 def Welcome():
     global Label1, Label2, Button1
@@ -126,13 +146,16 @@ def AdminLoginChecker():
             BlockingLabel = tkinter.Label(AdminWindow , text= "\t   System Blocked" , bg="white" ,fg = "red" , font= "Impact 30" )
             BlockingLabel.grid(row=7 , column= 2)
         else:
+            if Counter < 2 :
+                TriesLabel.destroy()
             TriesLabel = tkinter.Label(AdminWindow , text= "Remaining Tries " + str(Counter)  ,  bg="white" ,fg = "red" , font= "Impact 24" )
             TriesLabel.grid(row = 2 , column= 2)
             Counter-=1
 
 
 def AdminMenuDisplay():
-    global photo , VarList
+    global photo , VarList ,Counter , VarList
+    Counter = 2
     Label1 = tkinter.Label(AdminWindow,image = photo , width= 800 )
     Label1.pack()
     AdminLabel = tkinter.Label(AdminWindow , text= "Welcome to Admin Mode" , bg= "white" , fg = "red" , font= "Impact 24")
@@ -142,8 +165,9 @@ def AdminMenuDisplay():
     Option = ('Manage Patients' , 'Manage Doctors' , 'Book an Appointment')
     FeatureLabel = tkinter.Label(AdminWindow , text= "Please choose the feature:  " , bg= "white" , fg = "dodgerblue" , font= "Impact 18")
     FeatureLabel.pack(side='left')
-    OptionMenu= tkinter.OptionMenu(AdminWindow , VarList , *Option ).pack(ipadx = 100 , ipady= 10, side = 'left')
-    ConfirmButton = tkinter.Button(AdminWindow , text = "Confirm" , bg= "dimgray" , fg= "dodgerblue" , font = "Impact 18")
+    OptionMenu= tkinter.OptionMenu(AdminWindow , VarList , *Option )
+    OptionMenu.pack(ipadx = 100 , ipady= 10, side = 'left')
+    ConfirmButton = tkinter.Button(AdminWindow , text = "Confirm" , bg= "dimgray" , fg= "dodgerblue" , font = "Impact 18", command= ConfirmFeature)
     ConfirmButton.pack(ipadx = 20 , side = 'left' )
     # ManagePatientsButton = tkinter.Button(AdminWindow , text = "Manage Patients" , bg = "dimgray" , fg = "dodgerblue" , font = "Impact 24" )
     # ManagePatientsButton.pack()
@@ -153,6 +177,132 @@ def AdminMenuDisplay():
     # BookanAppointmentButton.pack()
 
 
+def ConfirmFeature():
+    global OptionMenu
+    
+    AdminChoice = VarList.get()
+    print(AdminChoice)
+    if AdminChoice == 'Manage Patients':
+        ManagePatients()
+
+
+def ManagePatients():
+    global AdminWindow  , PatientPhoto , PatientsWindow
+    PatientsWindow = tkinter.Toplevel(AdminWindow)
+    PatientsWindow.geometry("800x600+750+350")
+    PatientsWindow.title("Manage Patients")
+    PatientsWindow.iconbitmap(r"Icon.ico")
+    PatientsWindow.configure(background="white")
+    Patient_Pic = Image.open("ManagePatient.jpg")
+    resize = Patient_Pic.resize((800,150), Image.ANTIALIAS)
+    PatientPhoto = ImageTk.PhotoImage(resize)
+    Label1 = tkinter.Label(PatientsWindow,image = PatientPhoto , width= 800 )
+    Label1.pack()
+    ManagePatientLabel = tkinter.Label(PatientsWindow, text = "Manage Patients\n" ,bg ="White" , fg="red" , font = "Impact 24 ")
+    ManagePatientLabel.pack()
+    AddButton = tkinter.Button(PatientsWindow , text = "Add Patient" , bg = "dimgray" , fg = "dodgerblue" , font= "Impact 18" ,width= 30 , command= AddPatient )
+    AddButton.pack()
+    FreeLabel = tkinter.Label(PatientsWindow , text = "\n" , bg = "white")
+    FreeLabel.pack()
+    EditButton = tkinter.Button(PatientsWindow , text = "Edit Patient" , bg = "dimgray" , fg = "dodgerblue" , font= "Impact 18" ,width= 30 )
+    EditButton.pack()
+    FreeLabel = tkinter.Label(PatientsWindow , text = "\n" , bg = "white")
+    FreeLabel.pack()
+    DelButton = tkinter.Button(PatientsWindow , text = "Delete Patient" , bg = "dimgray" , fg = "dodgerblue" , font= "Impact 18" ,width= 30 )
+    DelButton.pack()
+    FreeLabel = tkinter.Label(PatientsWindow , text = "\n" , bg = "white")
+    FreeLabel.pack()
+    DisplayButton = tkinter.Button(PatientsWindow , text = "Display a patient" , bg = "dimgray" , fg = "dodgerblue" , font= "Impact 18" ,width= 30 )
+    DisplayButton.pack()
+
+
+def AddPatient():
+    global PatientsWindow , DoctorVar , DoctorName , DepartmentVar, GenderVar , DoctorMenu , AddPatientWindow
+    AddPatientWindow = tkinter.Toplevel(PatientsWindow)
+    AddPatientWindow.geometry("700x500+800+400")
+    AddPatientWindow.title("Add Patient")
+    AddPatientWindow.iconbitmap(r"Icon.ico")
+    AddPatientWindow.configure(background="white")
+    # i = 0
+    # while i < 12:
+    #     AddPatientWindow.grid_columnconfigure(i,minsize = 10)
+    #     AddPatientWindow.grid_rowconfigure(i,minsize = 10)
+    #     i+=1
+    # AddPatientLabel = tkinter.Label(AddPatientWindow , text = "Add Patient" , bg= "white" , fg= "red" ,font= "Impact 24" )
+    # AddPatientLabel.place(relx=0.5 , rely = 0 , anchor= N)
+    DepartmentLabel = tkinter.Label(AddPatientWindow , text = "Department: " , bg= "white" , fg = "dodgerblue" , font= "Impact 18")
+    DepartmentLabel.place(relx=0.01 , rely = 0.05 , anchor= W)
+    DepartmentVar = tkinter.StringVar(AddPatientWindow)
+    DepartmentVar.set("Anesthetics")
+    DepartmentMenu= tkinter.OptionMenu(AddPatientWindow , DepartmentVar , *DepartmentsStrip )
+    DepartmentMenu.place(relx=0.23 , rely = 0.05 , anchor= W , width= 300 , height= 30)
+    DoctorLabel = tkinter.Label(AddPatientWindow , text = "Doctor:" , bg = "white" , fg ="dodgerblue" , font = "Impact 18")
+    DoctorLabel.place(relx=0.01 , rely=0.15 ,anchor= W  )
+    DoctorVar = tkinter.StringVar(AddPatientWindow)
+    DoctorVar.set("Press Refresh After Choosing Department")
+    DoctorMenu = tkinter.OptionMenu(AddPatientWindow , DoctorVar ,*DoctorName)
+    DoctorMenu.place(relx=0.23 , rely = 0.15 , anchor= W , width= 300 , height= 30)
+    RefreshButton = tkinter.Button(AddPatientWindow , text="Refresh" , bg="dimgray" ,fg = "dodgerblue" , font = "Impact 12" , command= RefreshDoctor)
+    RefreshButton.place(relx=0.75 , rely = 0.15 , anchor= E )
+    NameLabel = tkinter.Label(AddPatientWindow ,text = "Patient Name: " , bg = "white" , fg ="dodgerblue" , font = "Impact 18")
+    NameLabel.place(relx=0.01 , rely= 0.25 , anchor= W)
+    NameEntry = tkinter.Entry(AddPatientWindow , bg= "lightgrey"  , font= "Impact 18",fg= "dodgerblue" ,width= 25 )
+    NameEntry.place(relx=0.23 , rely = 0.25 , anchor= W , width= 300 , height= 30)
+    AgeLabel = tkinter.Label(AddPatientWindow , text = "Age:" , bg= "White" , fg="dodgerblue" , font= "Impact 18" )
+    AgeLabel.place(relx=0.01 , rely = 0.35 , anchor= W)
+    AgeSpinbox = tkinter.Spinbox(AddPatientWindow , from_=  1 , to = 150 , bg ="lightgray" , fg= "dodgerblue" , font = "Impact 12")
+    AgeSpinbox.place(relx=0.23 , rely= 0.35 , anchor= W , width=300 , height= 30)
+    YearsLabel = tkinter.Label(AddPatientWindow , text= "Years" , bg="white" , fg="dodgerblue" , font="Impact 18")        
+    YearsLabel.place(relx=0.67 , rely= 0.35 , anchor=W)
+    GenderLabel = tkinter.Label(AddPatientWindow , text= "Gender:" ,bg="White" , fg= "dodgerblue" , font= "Impact 18")
+    GenderLabel.place(relx=0.01 , rely= 0.45 , anchor=W)
+    GenderVar = tkinter.StringVar(AddPatientWindow)
+    GenderVar.set("No Gender")
+    GenderMaleRadioButton = tkinter.Radiobutton(AddPatientWindow , text= "Male" , variable = GenderVar , value= "Male" ,bg="White" , fg ="dodgerblue" , font = "Impact 12")
+    GenderMaleRadioButton.place(relx=0.23 , rely= 0.45 , anchor=W)
+    GenderFemaleRadioButton = tkinter.Radiobutton(AddPatientWindow , text= "Female" , variable = GenderVar , value= "Female" ,bg="White" , fg ="dodgerblue" , font = "Impact 12")
+    GenderFemaleRadioButton.place(relx=0.43 , rely= 0.45 , anchor=W)
+    AddressLabel = tkinter.Label(AddPatientWindow , text = "Address:" , bg = "White" , fg = "dodgerblue" , font= "Impact 18")
+    AddressLabel.place(relx = 0.01 , rely=0.55 , anchor= W)
+    AddressEntry = tkinter.Entry(AddPatientWindow , bg= "lightgrey"  , font= "Impact 18",fg= "dodgerblue" ,width= 25 )
+    AddressEntry.place(relx=0.23 , rely = 0.55 , anchor= W , width= 300 , height= 30)
+    PhoneLabel = tkinter.Label(AddPatientWindow , text = "Phone Number:" , bg = "White" , fg = "dodgerblue" , font= "Impact 18")
+    PhoneLabel.place(relx=0.01 , rely = 0.65 , anchor= W)
+    PhoneEntry = tkinter.Entry(AddPatientWindow , bg= "lightgrey"  , font= "Impact 18",fg= "dodgerblue" ,width= 25 )
+    PhoneEntry.place(relx=0.23 , rely = 0.65 , anchor= W , width= 300 , height= 30)
+    RoomLabel = tkinter.Label(AddPatientWindow , text = "Room Number:" , bg = "White" , fg = "dodgerblue" , font= "Impact 18")
+    RoomLabel.place(relx=0.01 , rely = 0.75 , anchor= W)
+    RoomSpinBox = tkinter.Spinbox(AddPatientWindow , from_=  1000 , to = 10000 , bg ="lightgray" , fg= "dodgerblue" , font = "Impact 12")
+    RoomSpinBox.place(relx=0.23 , rely = 0.75 , anchor= W , width= 300 , height= 30)
+    DescribeLabel = tkinter.Label(AddPatientWindow , text = "Describtion:" , bg = "White" , fg = "dodgerblue" , font= "Impact 18")
+    DescribeLabel.place(relx = 0.01 , rely= 0.85 , anchor= W)
+    DescribeText = tkinter.Text(AddPatientWindow , bg= "lightgrey"  , font= "Impact 12",fg= "dodgerblue" ,width= 25)
+    DescribeText.place(relx=0.23 , rely = 0.85 , anchor= W , width= 300 , height= 30)
+    IDLabel = tkinter.Label(AddPatientWindow , text = "ID:" , bg = "White" , fg = "dodgerblue" , font= "Impact 18")
+    IDLabel.place(relx=0.01 , rely = 0.95 , anchor= W )
+    IDSpinBox = tkinter.Spinbox(AddPatientWindow , from_=  1 , to = 10000 , bg ="lightgray" , fg= "dodgerblue" , font = "Impact 12")
+    IDSpinBox.place(relx=0.23 , rely = 0.95 , anchor= W , width= 300 , height= 30)
+    PatientEnterButton = tkinter.Button(AddPatientWindow , text= "Enter" , bg ="dimgray" ,fg="dodgerblue" , font = "Impact 18")
+    PatientEnterButton.place(relx= 0.8 , rely= 0.9, anchor=W)
+
+
+def RefreshDoctor():
+    global DoctorMenu , AddPatientWindow , DoctorVar
+    if 'Doctor' not in Database[DepartmentVar.get()]:
+        DoctorName.clear()
+        DoctorName.append("There's no available Doctors")
+        DoctorMenu.destroy()
+        DoctorVar.set("There's no available Doctors")
+        DoctorMenu = tkinter.OptionMenu(AddPatientWindow , DoctorVar ,*DoctorName)
+        DoctorMenu.place(relx=0.23 , rely = 0.15 , anchor= W , width= 300 , height= 30)
+    else:
+        DoctorName.clear()
+        for i in Database[DepartmentVar.get()]['Doctor']:
+            DoctorName.append("Dr. " + i["Name"])
+        DoctorMenu.destroy()
+        DoctorVar.set(DoctorName[0])
+        DoctorMenu = tkinter.OptionMenu(AddPatientWindow , DoctorVar ,*DoctorName)
+        DoctorMenu.place(relx=0.23 , rely = 0.15 , anchor= W , width= 300 , height= 30)
 
 
 root = tkinter.Tk()
